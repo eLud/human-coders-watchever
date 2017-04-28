7d1460a75c77ac9bcec638b43bc34452781b8fe2
Ludovic Ollagnier <ludovic@tectec.fr>
2017-04-28 17:30:18 +0200

    URLSession demo

--- Awesome Points Of Interests/PoiDetailsViewController.swift
+++ Awesome Points Of Interests/PoiDetailsViewController.swift
@@ -12,12 +12,42 @@ class PoiDetailsViewController: UIViewController {
     
     var currentPoi: PointOfInterest?
 
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var imageView2: UIImageView!
+
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
         
         self.title = currentPoi?.name
+                
+        let url = URL(string: "https://developer.apple.com/swift/images/swift-og.png")!
+        
+        let task = URLSession.shared.dataTask(with: url) { (data, response, error) in
+            
+            print("OK")
+            guard error == nil else { return }
+            guard let data = data else { return }
+            
+            let image2 = UIImage(data: data)
+            self.imageView2.image = image2
+            
+            DispatchQueue.main.async {
+                let image = UIImage(data: data)
+                self.imageView.image = image
+            }
+
+            
+        }
+        task.resume()
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+        self.navigationController?.navigationBar.barTintColor = .green
     }
 
     override func didReceiveMemoryWarning() {
